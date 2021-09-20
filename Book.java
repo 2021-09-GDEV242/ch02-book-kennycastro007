9d0d8d0ce6afd5cf6e6a4f205d59f71dd69eda44
Holy Bubbles <holybubbles00@gmail.com>
2021-09-19 20:06:09 -0400

    2.83

--- Book.java
+++ Book.java
@@ -3,13 +3,29 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @author Kenny Castro-Monroy
+ * @version 09.19.2021
  */
 class Book
 {
     // The fields.
     private String author;
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     private String title;
 
     /**
